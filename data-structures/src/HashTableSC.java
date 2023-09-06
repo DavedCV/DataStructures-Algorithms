4718b400a283ed749f1ba91bfe5345dde6ec8d97
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-09-06 11:43:31 -0500

    - Hash table with separate chaining method completed

--- data-structures/src/HashTableSC.java
+++ data-structures/src/HashTableSC.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 class Entry<Key, Value> {
 
@@ -13,7 +11,7 @@ class Entry<Key, Value> {
         this.value = value;
 
         // use builtin method to compute the hashcode, or use overrided hashcode
-        this.hash = Key.hashCode();
+        this.hash = key.hashCode();
     }
 
     public boolean equals(Entry<Key, Value> other) {
@@ -36,11 +34,11 @@ public class HashTableSC<Key, Value> implements Iterable<Key> {
     // used to keep track of load factor, in order to manage table resizing
     private double maxLoadFactor;
     // how big is the table
-    private int capacity = 0;
+    private int capacity ;
     // capacity * max load factor
-    private int threshold = 0;
+    private int threshold;
     // actual number of entries in the table
-    private int size = 0;
+    private int size;
     // table
     private LinkedList<Entry<Key, Value>>[] table;
 
@@ -53,7 +51,7 @@ public class HashTableSC<Key, Value> implements Iterable<Key> {
     }
 
     public HashTableSC(int capacity, double maxLoadFactor) {
-        if (initialCapacity < 0)
+        if (capacity < 0)
             throw new IllegalArgumentException("Illegal Capacity");
         if (maxLoadFactor <= 0 || Double.isNaN(maxLoadFactor) || Double.isInfinite(maxLoadFactor))
             throw new IllegalArgumentException("Illegal load factor");
@@ -88,14 +86,14 @@ public class HashTableSC<Key, Value> implements Iterable<Key> {
 
     // returns true depending on whether a key is in the hash table
     public boolean hasKey(Key key) {
-        int tableBucketIndex = normalizeIndex(key.hashCode());
-        return seekBucketEntry(tableBucketIndex, key) != null;
+        int bucketIndex = normalizeIndex(key.hashCode());
+        return seekBucketEntry(bucketIndex, key) != null;
     }
 
     // finds and returns a particular entry in a given bucket if it exists, otherwise returns null
-    private Entry<Key, Value> seekBucketEntry(int tableBucketIndex, Key key) {
+    private Entry<Key, Value> seekBucketEntry(int bucketIndex, Key key) {
 
-        LinkedList<Entry<Key, Value>> tableBucket = table[tableBucketIndex];
+        LinkedList<Entry<Key, Value>> tableBucket = table[bucketIndex];
         if (tableBucket == null) return null;
 
         for (Entry<Key, Value> entry : tableBucket)
@@ -104,6 +102,7 @@ public class HashTableSC<Key, Value> implements Iterable<Key> {
         return null;
     }
 
+    // ad a value to the hash table
     public Value add(Key key, Value value) {
         if (key == null) throw new IllegalArgumentException("Null Key");
 
@@ -115,7 +114,7 @@ public class HashTableSC<Key, Value> implements Iterable<Key> {
 
     // add a new entry to a bucket in the table if the entry does not already exists
     // in the given bucket, but if it does, update value
-    // return null or previous entry value if the data is updated
+    // return null if there aren´t previous value, or previous entry value if the data is updated
     private Value bucketInsertEntry(int bucketIndex, Entry<Key, Value> newEntry) {
         LinkedList<Entry<Key, Value>> tableBucket = table[bucketIndex];
         if (tableBucket == null)
@@ -135,8 +134,138 @@ public class HashTableSC<Key, Value> implements Iterable<Key> {
 
     }
 
+    // return the stored value of the key provided
+    // returns null if the key doesn't exist, or null if the value is exactly null
+    public Value get (Key key) {
+        if (key == null) return null;
+
+        int bucketIndex = normalizeIndex(key.hashCode());
+        Entry<Key, Value> entry = seekBucketEntry(bucketIndex, key);
+
+        if (entry != null) return entry.value;
+
+        return null;
+    }
+
+    // remove a entry from the table given a key
+    // return the value of the removed entry, or null if that entry not exists
+    public Value remove(Key key) {
+        if (key == null) return null;
+        int bucketIndex = normalizeIndex(key.hashCode());
+        return bucketRemoveEntry(bucketIndex, key);
+    }
+
+    private Value bucketRemoveEntry(int bucketIndex, Key key) {
+        LinkedList<Entry<Key, Value>> bucket = table[bucketIndex];
+
+        Entry<Key, Value> entry = seekBucketEntry(bucketIndex, key);
+
+        if (entry == null) return null;
+
+        bucket.remove(entry);
+        size--;
+        return entry.value;
+    }
+
+    // resizes the internal table holding buckets of entries
+    private void resizeTable() {
+
+        capacity *= 2;
+        threshold = (int) (capacity * maxLoadFactor);
+
+        LinkedList<Entry<Key, Value>>[] newTable = new LinkedList[capacity];
+
+        for (LinkedList<Entry<Key, Value>> list : table) {
+            if (list != null) {
+                for (Entry<Key, Value> entry : list) {
+                    int bucketIndex = normalizeIndex(entry.hash);
+                    LinkedList<Entry<Key, Value>> bucket = newTable[bucketIndex];
+                    if (bucket == null) newTable[bucketIndex] = bucket = new LinkedList<>();
+                    bucket.add(entry);
+                }
+
+                list.clear();
+            }
+        }
+
+        // avoid memory leak
+        table = newTable;
+    }
+
+    // return all the keys of the hash table
+    public List<Key> keys() {
+
+        List<Key> keys = new ArrayList<>(size);
+        for (LinkedList<Entry<Key, Value>> bucket : table)
+            if (bucket != null)
+                for (Entry<Key, Value> entry : bucket)
+                    keys.add(entry.key);
+
+        return keys;
+    }
+
+    // return all the values within the hash table
+    public List<Value> values() {
+
+        List<Value> values = new ArrayList<>(size);
+        for (LinkedList<Entry<Key, Value>> bucket : table)
+            if (bucket != null)
+                for (Entry<Key, Value> entry : bucket)
+                    values.add(entry.value);
+
+        return values;
+    }
+
     @Override
     public Iterator<Key> iterator() {
-        return null;
+
+        final int elementCount = size();
+
+        return new Iterator<>() {
+
+            int bucketIndex = 0;
+            Iterator<Entry<Key, Value>> bucketIterator = (table[bucketIndex] == null) ? null : table[bucketIndex].iterator();
+
+            @Override
+            public boolean hasNext() {
+
+                // no iterator or current iterator is empty
+                if (bucketIterator == null || !bucketIterator.hasNext()) {
+
+                    // search next buckets until a valid iterator is found
+                    while (++bucketIndex < capacity) {
+
+                        if (table[bucketIndex] != null) {
+
+                            Iterator<Entry<Key, Value>> nextIterator = table[bucketIndex].iterator();
+                            // make sure the iterator actually has elements
+                            if (nextIterator.hasNext()) {
+                                bucketIterator = nextIterator;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return bucketIndex < capacity;
+            }
+
+            @Override
+            public Key next() {
+                return bucketIterator.next().key;
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (int i = 0; i < capacity; i++) {
+            if (table[i] == null) continue;
+            for (Entry<Key, Value> entry : table[i]) sb.append(entry).append(", ");
+        }
+        sb.append("}");
+        return sb.toString();
     }
 }
